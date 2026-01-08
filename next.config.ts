d00110999b092133577380dfff128e8e052ad607
MOCEVN <mocevn@outlook.com>
2026-01-08 13:09:16 +0100

    add: docker

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'static.photos',
-        pathname: '/**',
-      },
-    ],
-  },
+	output: "standalone",
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "static.photos",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
